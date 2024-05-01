7282ffcc1f65f91b63597a3d003d42a26f0c413a
hrenikindi <159125359+hrenikindi@users.noreply.github.com>
2024-05-01 13:58:05 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+function Container() {
+    return (
+      <div>
+        Hey Kalvians! Welcome to React Learning
+        <div>Let's rock and roll</div>
+      </div>
+    );
+}
+const container = document.getElementById('react-container');
+ReactDOM.render(<Container />, container);
+  
